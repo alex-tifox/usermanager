9a74cc9249f5f3245544eda6cc93d04c0a0c4668
mrFox <tilnyi.olexander@gmail.com>
2017-11-27 22:28:08 +0100

    configuration finished

--- src/main/java/manager/controller/UserController.java
+++ src/main/java/manager/controller/UserController.java
@@ -1,4 +1,11 @@
 package manager.controller;
 
+import manager.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
 }
